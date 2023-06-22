cd08f4f1997db8c0cff5dd481b597ca8f13a94dd
Mohdsofi1 <mestilahhh@gmail.com>
2023-06-22 21:32:16 +0800

    admin

--- controller/auth/register.js
+++ controller/auth/register.js
@@ -8,7 +8,7 @@ const register = async (req, res) => {
     const salt = bcrypt.genSaltSync(10);
     const hashedPassword = bcrypt.hashSync(body.password, salt);
     const dbRes = await query(
-      "INSERT INTO users (email, username, password, isadmin) VALUES ($1, $2, $3, $4)",
+      "INSERT INTO users (email, username, password, is_admin) VALUES ($1, $2, $3, $4)",
       [body.email, body.username, hashedPassword, body.isadmin]
     );
     const serverRes = {
